40f67a3d2edf8397b515e22c493149679b48243e
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-05-07 23:09:49 +0530

    user route done

--- src/app/api/create-user/route.js
+++ src/app/api/create-user/route.js
@@ -1,3 +1,29 @@
 export async function POST(request) {
-  const { name, email, password } = await request.json();
+  const { body } = await request.json();
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+  if (!userExists) {
+    return Response.json(JSON.stringify({ message: "User already exists" }), {
+      status: 400,
+    });
+  }
+  const userCreated = await prisma.user.create({
+    name: body.name,
+    email: body.email,
+    password: body.password,
+  });
+  if (!userCreated) {
+    return Response.json(JSON.stringify({ message: "User not created" }), {
+      status: 500,
+    });
+  }
+  return Response.json(
+    JSON.stringify({ message: "User created successfully" }),
+    {
+      status: 201,
+    },
+  );
 }
